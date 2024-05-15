cb79deb8e576ae779e15db1086c95b39fb3684c3
Chukiat Jaydeestan <chukiat.ja@nocnoc.com>
2024-05-15 10:02:03 +0700

    initial project

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by SatangBiiger Jaydeestan on 13/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
